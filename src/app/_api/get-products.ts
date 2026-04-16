2f6418687d909632b9b1f588bb14e6457b5a4128
Vitali Shatsou <vitali_shatsou@epam.com>
2026-04-16 19:48:31 +0200

    chore: add cache only for case without category and search

--- src/app/_api/get-products.ts
+++ src/app/_api/get-products.ts
@@ -5,15 +5,27 @@ import "server-only";
 import { ListProductsCategoryEnum, productsApi } from "@/lib/api-client";
 import { cacheLife, cacheTag } from "next/cache";
 
+async function getProductsAll() {
+  "use cache";
+  cacheTag(`products`);
+  cacheLife("hours");
+  cacheTag(`products-all`);
+
+  try {
+    return await productsApi.listProducts({
+      limit: 5,
+    });
+  } catch {
+    return { success: false, data: [] };
+  }
+}
+
 export async function getProducts(
   search: string,
   categorySlug?: ListProductsCategoryEnum,
 ) {
-  "use cache";
-  cacheTag(`products`);
   if (search === "" && !categorySlug) {
-    cacheLife("hours");
-    cacheTag(`products-all`);
+    return await getProductsAll();
   }
 
   try {
